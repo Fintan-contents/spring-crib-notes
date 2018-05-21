c741981d8672fb9af379816ecbd4da044fbd1ed6
mizutani.wataru.569 <mizutani.wataru.569@intra.aws-axion.net>
2018-05-21 10:07:06 +0900

    http response header用のサンプルプロジェクトを追加

---
+++ samples/web/http-response-header/src/main/java/keel/HttpResponseHeaderApplication.java
@@ -0,0 +1,11 @@
+package keel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpResponseHeaderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HttpResponseHeaderApplication.class, args);
+    }
+}
