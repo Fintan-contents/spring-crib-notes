151c2c72f9bbdaa7a7fb779f3a771fd326a05270
mizutani.wataru.569 <mizutani.wataru.569@intra.aws-axion.net>
2018-05-21 10:42:52 +0900

    pom.xmlの整備と簡単な画面を追加

--- samples/web/http-response-header/src/main/java/keel/HttpResponseHeaderApplication.java
+++ samples/web/http-response-header/src/main/java/keel/HttpResponseHeaderApplication.java
@@ -2,10 +2,18 @@ package keel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@Controller
 public class HttpResponseHeaderApplication {
     public static void main(String[] args) {
         SpringApplication.run(HttpResponseHeaderApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String get() {
+        return "index";
     }
 }
