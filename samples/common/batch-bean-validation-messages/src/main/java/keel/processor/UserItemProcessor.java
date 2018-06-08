a653843b84382e825cf6f3cd9f01309c18b518b6
Wataru Mizutani <mizutani.wataru.569@tis.co.jp>
2018-06-08 09:26:05 +0900

    fix https://cq.keel-dev.net/redmine/issues/296 (#62)
    fix https://cq.keel-dev.net/redmine/issues/296

--- samples/common/batch-bean-validation-messages/src/main/java/keel/processor/UserItemProcessor.java
+++ samples/common/batch-bean-validation-messages/src/main/java/keel/processor/UserItemProcessor.java
@@ -1,19 +1,22 @@
 package keel.processor;
 
-import keel.entity.User;
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.stereotype.Component;
 
-import javax.validation.ConstraintViolation;
-import javax.validation.Validator;
-import java.util.Set;
+import keel.entity.User;
 
 @Component
 public class UserItemProcessor implements ItemProcessor<User, User> {
 
     private final Validator validator;
+
     private final Logger logger = LoggerFactory.getLogger(UserItemProcessor.class);
 
     public UserItemProcessor(Validator validator) {
@@ -22,24 +25,20 @@ public class UserItemProcessor implements ItemProcessor<User, User> {
 
     @Override
     public User process(final User user) throws Exception {
-
         final Set<ConstraintViolation<User>> results = validator.validate(user);
         if (!results.isEmpty()) {
-            results
-                    .forEach(
-                            userConstraintViolation ->
-                                    logger.warn(
-                                            "column name：{}, error message：{}",
-                                                    userConstraintViolation
-                                                            .getPropertyPath(),
-                                                    userConstraintViolation.getMessage()
-                                    )
-                    );
+            results.forEach(this::writeValidationErrorLog);
             return null;
         }
-
         return new User(
-                user.getUserId(), user.getUserName() + "様");
+                user.getUserId(), user.getUserName() + '様');
+    }
+
+    private void writeValidationErrorLog(final ConstraintViolation<User> constraintViolation) {
+        logger.warn("column name：{}, error message：{}",
+                constraintViolation.getPropertyPath(),
+                constraintViolation.getMessage()
+        );
     }
 
 }
