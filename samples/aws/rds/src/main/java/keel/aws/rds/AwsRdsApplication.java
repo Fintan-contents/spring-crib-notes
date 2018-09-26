65ee7ce4fbe672073a0e082ab558591d2ba29b13
keel-dev <keel@tis.co.jp>
2018-09-26 14:45:17 +0900

    AWS RDSを使うサンプルを追加しました。

---
+++ samples/aws/rds/src/main/java/keel/aws/rds/AwsRdsApplication.java
@@ -0,0 +1,12 @@
+package keel.aws.rds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsRdsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsRdsApplication.class, args);
+    }
+}
---
+++ samples/aws/rds/src/main/resources/db/migration/V001__create_table.sql
@@ -0,0 +1,5 @@
+create table users (
+  id bigint not null auto_increment,
+  name varchar(255) not null,
+  primary key (id)
+);
