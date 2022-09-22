9052550aea6464833e399ee39682036859cbbff4
Shinya Hijiri <hijiri.shinya@tis.co.jp>
2022-09-22 17:28:29 +0900

    JUnit4->5対応のコミット漏れ

--- samples/common/sql-management/src/test/java/keel/sqlmanagement/SqlManagementApplicationTest.java
+++ samples/common/sql-management/src/test/java/keel/sqlmanagement/SqlManagementApplicationTest.java
@@ -1,11 +1,8 @@
 package keel.sqlmanagement;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest(classes = SqlManagementApplication.class)
 public class SqlManagementApplicationTest {
 
