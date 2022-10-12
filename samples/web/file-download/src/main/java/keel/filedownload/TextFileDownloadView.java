bcc16201d7b3b0ebfe9c371a2af8847b04f24a1e
Shinya Hijiri <hijiri.shinya@tis.co.jp>
2022-10-12 15:30:11 +0900

    ファイルダウンロードのコードを見直し

--- samples/web/file-download/src/main/java/keel/filedownload/TextFileDownloadView.java
+++ samples/web/file-download/src/main/java/keel/filedownload/TextFileDownloadView.java
@@ -1,8 +1,8 @@
 package keel.filedownload;
 
+import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StreamUtils;
 import org.springframework.web.servlet.view.AbstractView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -20,22 +20,21 @@ public class TextFileDownloadView extends AbstractView {
 
     public TextFileDownloadView(ResourceLoader resourceLoader) {
         this.resourceLoader = resourceLoader;
+        setContentType("text/plain");
     }
 
     @Override
     protected void renderMergedOutputModel(Map<String, Object> model,
                                            HttpServletRequest request, HttpServletResponse response) throws Exception {
-
-        final FileDownloadAttributes attributes = (FileDownloadAttributes) model.get(DOWNLOAD_FILE_INFO_KEY);
+        // ダウンロード対象のファイルを読込み、レスポンスボディに書込みます
+        FileDownloadAttributes attributes = (FileDownloadAttributes) model.get(DOWNLOAD_FILE_INFO_KEY);
+        Resource downloadFileResource = resourceLoader.getResource(attributes.getTargetFilePath());
+        long size = downloadFileResource.getInputStream().transferTo(response.getOutputStream());
 
         // レスポンスヘッダを設定します
+        response.setContentType(getContentType());
         response.setHeader("Content-Disposition", "attachment; filename=" + attributes.getDownloadFileName());
-        response.setHeader("Content-Type", "text/plain");
-
-        // ダウンロード対象のファイルを読込み、レスポンスボディに書込みます
-        StreamUtils.copy(
-                resourceLoader.getResource(attributes.getTargetFilePath()).getInputStream(),
-                response.getOutputStream());
+        response.setHeader("Content-Length", String.valueOf(size));
     }
 }
 // example-end
