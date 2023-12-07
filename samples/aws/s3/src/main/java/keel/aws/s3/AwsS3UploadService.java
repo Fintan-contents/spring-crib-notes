7a7cd46e44e54ba329c3dafe052ba660157def31
hijiri <3445928+hiji@users.noreply.github.com>
2023-12-08 08:51:45 +0900

    S3連携実装例をSpring AWS Cloud 3に対応

--- samples/aws/s3/src/main/java/keel/aws/s3/AwsS3UploadService.java
+++ samples/aws/s3/src/main/java/keel/aws/s3/AwsS3UploadService.java
@@ -1,15 +1,13 @@
 package keel.aws.s3;
 
 import java.io.IOException;
-import java.io.OutputStream;
 import java.io.UncheckedIOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+import io.awspring.cloud.s3.S3Template;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.core.io.ResourceLoader;
-import org.springframework.core.io.WritableResource;
 import org.springframework.stereotype.Service;
 
 // upload-start
@@ -18,30 +16,24 @@ public class AwsS3UploadService {
 
     private final Logger logger = LoggerFactory.getLogger(AwsS3UploadService.class);
 
-    private final ResourceLoader resourceLoader;
+    private final S3Template s3Template;
 
     private final AwsS3Properties properties;
 
-    public AwsS3UploadService(ResourceLoader resourceLoader, AwsS3Properties properties) {
-        this.resourceLoader = resourceLoader;
+    public AwsS3UploadService(S3Template s3Template, AwsS3Properties properties) {
+        this.s3Template = s3Template;
         this.properties = properties;
     }
 
     public void uploadFile(Path path) {
         logger.info("{}をS3にアップロードします。", path.getFileName());
 
-        WritableResource writableResource = (WritableResource) resourceLoader.getResource(createObjectLocation(path));
-        try (OutputStream outputStream = writableResource.getOutputStream()) {
-            Files.copy(path, outputStream);
-
+        try {
+            s3Template.upload(properties.getBucketName(), path.getFileName().toString(), Files.newInputStream(path));
             logger.info("ファイルのアップロードに成功しました。");
         } catch (IOException e) {
             throw new UncheckedIOException("S3へのファイルアップロードに失敗しました。", e);
         }
-    }
-
-    private String createObjectLocation(Path path) {
-        return "s3://" + properties.getBucketName() + "/upload/" + path.getFileName();
     }
 }
 // upload-end
